e1e5f7fda4316414b18001c38d7dae32fd709e7d
Nicholas Powers <njpowers7915@gmail.com>
2018-07-21 17:09:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function setBestCustomer() {
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
 }
+
+const leastFavoriteCustomer = "Nick";
+
+function changeLeastFavoriteCustomer() {
+
+}
